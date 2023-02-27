389aa319476471dc04001899dcf61328ffc58ef3
shashankg32 <shashank6511@gmail.com>
2023-02-27 23:01:44 +0530

    using arrow function, video no 416

--- src/index.js
+++ src/index.js
@@ -1,33 +1,30 @@
-// var numbers = [3, 56, 2, 48, 5];
-// //foreach
-// var newNumbers = [];
-// numbers.forEach(function double(x){
-//   newNumbers.push(x*2);
-// }
-// );
-// console.log(newNumbers)
-// //filter
-// const newNumberss = numbers.filter(
-// function (num)
-//   {
-//     return num > 10;
-//   }
-// );
+
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./components/App";
+
+//var numbers = [3, 56, 2, 48, 5];
+//foreach
+// var newNumbers = []; numbers.forEach((x) => newNumbers.push(x*2);}); console.log(newNumbers);
+//map
+// const newNumbers = numbers.map(x=>x*2);
+// console.log(newNumbers);
+//filter
+// const newNumberss = numbers.filter(num => num < 10);
 // console.log(newNumberss)
-// //reduce
-// var newNumbersss=numbers.reduce(function(acc,currnum)
-// {
-//   return acc + currnum
-// })
+//reduce
+// var newNumbersss=numbers.reduce((acc,currnum) => acc+currnum);
 // console.log(newNumbersss)
-// //find
-// const newNumberssss=newNumbers.find(function(num)
-// {return num>10;})
-// console.log(newNumberssss)
+//find : only shows the first value in array
+// const newNumberssss = numbers.find(num=> num > 10);
+// console.log(newNumberssss);
+//find-index : only shows the first value index in array
+// const newNumbersssss = numbers.findIndex(num => num > 10);
+// console.log(newNumbersssss);
 
 import emojipedia from "./emojipedia";
-
 const newemojipedia = emojipedia.map(function(emojiEntry){
-  return emojiEntry.meaning.substring(0,100)
-})
+return emojiEntry.meaning.substring(0,100)})
 console.log(emojipedia);
+
+ReactDOM.render(<App />, document.getElementById("root"));
